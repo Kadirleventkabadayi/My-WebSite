b1a9714cfbd47302732bc9d7258f05e2877766f7
Prelencos <120386410+Kadirleventkabadayi@users.noreply.github.com>
2025-01-27 14:49:47 +0300

    Add TopSelectedCard component to enhance card display on Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Box } from "@mui/material";
 import SelectedCard from "./components/cards/SelectedCard";
+import TopSelectedCard from "./components/cards/TopSelectedCard";
 
 export default function Home() {
   return (
     <Box>
+      <TopSelectedCard />
       {[1, 2, 3, 4, 5].map((item) => (
         <SelectedCard key={item} />
       ))}
