96cb15396ea26f7258fc03dd5f22f46ddd10af1d
Prelencos <120386410+Kadirleventkabadayi@users.noreply.github.com>
2025-01-29 15:53:59 +0300

    Refactor TopCategories to use technologies array and enhance CategoryCard with description

--- src/app/lib/consts.ts
+++ src/app/lib/consts.ts
@@ -1,1 +1,47 @@
-export const imgList = ["/dashboard.png", "/mylib.png", "/blog.png"];
+const imgList = ["/dashboard.png", "/mylib.png", "/blog.png"];
+const technologies = [
+  {
+    title: "React",
+    image: "/React.png",
+    description:
+      "React is an open-source JavaScript library for building user interfaces. It allows for the creation of reusable components and facilitates the development of dynamic, high-performance web applications.",
+  },
+  {
+    title: "Next.js",
+    image: "/next.png",
+    description:
+      "Next.js is a React-based framework that enables server-side rendering (SSR) and static site generation (SSG). It optimizes web performance and is ideal for SEO-friendly and fast-loading web applications.",
+  },
+  {
+    title: "JavaScript",
+    image: "/JS.png",
+    description:
+      "JavaScript is a dynamic programming language that runs in web browsers. It enables interactivity and dynamic content on web pages and is essential for both frontend and backend web development.",
+  },
+  {
+    title: "TypeScript",
+    image: "/TS.png",
+    description:
+      "TypeScript is a superset of JavaScript that adds static typing and object-oriented programming features. It helps developers build large-scale applications with better tooling and error-checking during development.",
+  },
+  {
+    title: "Tailwind CSS",
+    image: "/tailwind_css.png",
+    description:
+      "Tailwind CSS is a utility-first CSS framework that provides low-level utility classes to build custom designs quickly. It allows for fast styling and easy customization of layouts.",
+  },
+  {
+    title: "Node.js",
+    image: "/Node.png",
+    description:
+      "Node.js is an open-source runtime environment that allows developers to run JavaScript on the server-side. It's ideal for building scalable and high-performance applications, especially for real-time applications.",
+  },
+  {
+    title: "Java Spring",
+    image: "/Spring.png",
+    description:
+      "Spring is a Java-based framework used for building enterprise-level web applications. It provides a comprehensive infrastructure for security, data management, and service-oriented architecture.",
+  },
+];
+
+export { imgList, technologies };
