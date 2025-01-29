202307c5ae1cda460d68787631330386fd16e12e
Prelencos <120386410+Kadirleventkabadayi@users.noreply.github.com>
2025-01-29 14:03:26 +0300

    Refactor TopArea component layout and enhance display of repository data

--- src/app/components/areas/TopArea.tsx
+++ src/app/components/areas/TopArea.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import TopSelectedCard from "../cards/TopSelectedCard";
 import { getColorByString } from "@/app/lib/utils";
 import SelectedCard from "../cards/SelectedCard";
@@ -21,41 +21,52 @@ function TopArea({ id, repoData }: TopAreaProps) {
     <Box
       id={id}
       sx={{
-        alignItems: "center",
-        justifyContent: "center",
         display: "flex",
-        gap: 2,
+        flexDirection: "column",
+        alignItems: "center",
+        paddingInline: 5,
+
         backgroundColor: "var(--topArea)",
         height: "95vh",
       }}
     >
-      <TopSelectedCard
-        title={erciyesAIContentCreator?.name || ""}
-        cardType={getColorByString(erciyesAIContentCreator?.name || "")}
-        description={erciyesAIContentCreator?.name}
-        updated_at={erciyesAIContentCreator?.updated_at || ""}
-        repoUrl={erciyesAIContentCreator?.html_url || ""}
-      />
+      <Typography variant="h1">Top Area</Typography>
       <Box
         sx={{
+          alignItems: "center",
+          justifyContent: "center",
           display: "flex",
-          justifyContent: "space-between",
-          flexDirection: "column",
-          height: "80vh",
+          gap: 2,
         }}
       >
-        {otherRepoData
-          .filter((_, index) => [0, 4, 3].includes(index))
-          .map((item) => (
-            <SelectedCard
-              title={item.name}
-              cardType={getColorByString(item.name)}
-              key={item.name}
-              desciption={item.name}
-              updated_at={item.updated_at}
-              repoUrl={item.html_url}
-            />
-          ))}
+        <TopSelectedCard
+          title={erciyesAIContentCreator?.name || ""}
+          cardType={getColorByString(erciyesAIContentCreator?.name || "")}
+          description={erciyesAIContentCreator?.name}
+          updated_at={erciyesAIContentCreator?.updated_at || ""}
+          repoUrl={erciyesAIContentCreator?.html_url || ""}
+        />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            flexDirection: "column",
+            height: "80vh",
+          }}
+        >
+          {otherRepoData
+            .filter((_, index) => [0, 4, 3].includes(index))
+            .map((item) => (
+              <SelectedCard
+                title={item.name}
+                cardType={getColorByString(item.name)}
+                key={item.name}
+                desciption={item.name}
+                updated_at={item.updated_at}
+                repoUrl={item.html_url}
+              />
+            ))}
+        </Box>
       </Box>
     </Box>
   );
