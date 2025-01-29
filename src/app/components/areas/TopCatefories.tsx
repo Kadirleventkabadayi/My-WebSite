8cf7e8d4e5f7a89eb462a9f45b519808fba7a868
Prelencos <120386410+Kadirleventkabadayi@users.noreply.github.com>
2025-01-29 14:34:42 +0300

    Update category titles and images in TopCategories; add new images for JavaScript, TypeScript, React, Next, Node.js, Tailwind CSS, and Java Spring

--- src/app/components/areas/TopCatefories.tsx
+++ src/app/components/areas/TopCatefories.tsx
@@ -7,40 +7,33 @@ import { useRef } from "react";
 
 const categories = [
   {
-    title: "JavaScript",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/225px-Unofficial_JavaScript_logo_2.svg.png",
-  },
-  {
-    title: "TypeScript",
-    image:
-      "https://w7.pngwing.com/pngs/915/519/png-transparent-typescript-hd-logo-thumbnail.png",
-  },
-  {
-    title: "Java Spring",
-    image:
-      "https://miro.medium.com/v2/resize:fit:624/1*dwa1SCG85BAzQttURVUvrA.png",
-  },
-  {
-    title: "Node.js",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6mkk0TKy0Hww7V1J9JkVUaHoF35GhtJN1Tw&s",
-  },
-  {
     title: "React",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTLnaWpiWTDkNU459gZ_p_KvqEe3YusUHguCFAB03VIR9hNzfIA8JiEzUrD8UAmKVOKk8",
+    image: "/React.png",
   },
   {
     title: "Next",
-    image:
-      "https://images.seeklogo.com/logo-png/44/2/next-js-logo-png_seeklogo-449824.png",
+    image: "/next.png",
   },
-  ,
+  {
+    title: "JavaScript",
+    image: "/JS.png",
+  },
+  {
+    title: "TypeScript",
+    image: "/TS.png",
+  },
   {
     title: "Tailwind CSS",
-    image:
-      "https://brandlogos.net/wp-content/uploads/2023/11/tailwind_css-logo_brandlogos.net_v91ni.png",
+    image: "/tailwind_css.png",
+  },
+  {
+    title: "Node.js",
+    image: "/Node.png",
+  },
+
+  {
+    title: "Java Spring",
+    image: "/Spring.png",
   },
 ];
 
