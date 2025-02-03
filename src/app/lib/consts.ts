c3f25b67a9e2e34b23ee3bcff6a58abe8decc76a
Prelencos <120386410+Kadirleventkabadayi@users.noreply.github.com>
2025-02-03 22:53:19 +0300

    Update technology descriptions to reflect current projects

--- src/app/lib/consts.ts
+++ src/app/lib/consts.ts
@@ -4,31 +4,31 @@ const technologies = [
     title: "React",
     image: "/React.png",
     description:
-      "With React, I developed interactive user interfaces for web applications using reusable components and various state management libraries. Here are a few of my projects: Projcet1, Project2, Project3.",
+      "With React, I developed interactive user interfaces for web applications using reusable components and various state management libraries. Here are a few of my projects: Admin Dashboard and My Library.",
   },
   {
     title: "Next.js",
     image: "/next.png",
     description:
-      "With Next.js, I built server-side rendered web applications with optimized performance and SEO. It allowed me to create dynamic pages and static sites with ease. Here are a few of my projects: Projcet1, Project2, Project3.",
+      "With Next.js, I built server-side rendered web applications with optimized performance and SEO. It allowed me to create dynamic pages and static sites with ease. Here are a few of my projects: AI Content Generator and Blog Site.",
   },
   {
     title: "JavaScript",
     image: "/JS.png",
     description:
-      "JavaScript is the my primary programming language for building web applications. I used it to create interactive features, dynamic content, and responsive layouts for various projects. Here are a few of my projects: Projcet1, Project2, Project3.",
+      "JavaScript is the my primary programming language for building web applications. I used it to create interactive features, dynamic content, and responsive layouts for various projects.",
   },
   {
     title: "TypeScript",
     image: "/TS.png",
     description:
-      "TypeScript is the language I prefer nowadays for building scalable and maintainable web applications. It provides static typing, interfaces, and other features that help catch errors early and improve code quality. Here are a few of my projects: Project 1, Project 2, Project 3.",
+      "TypeScript is the language I prefer nowadays for building scalable and maintainable web applications. It provides static typing, interfaces, and other features that help catch errors early and improve code quality.",
   },
   {
     title: "Tailwind CSS",
     image: "/tailwind_css.png",
     description:
-      "Tailwind CSS is the utility-first CSS framework I prefer nowadays for style web applications. It allowed me to create custom designs with minimal CSS code and responsive layouts. Here are a few of my projects: Project 1, Project 2, Project 3.",
+      "Tailwind CSS is the utility-first CSS framework I prefer nowadays for style web applications. It allowed me to create custom designs with minimal CSS code and responsive layouts.",
   },
   {
     title: "Node.js",
@@ -40,7 +40,7 @@ const technologies = [
     title: "Java Spring",
     image: "/Spring.png",
     description:
-      "Spring is a Java-based framework used for building enterprise applications. I used it to create RESTful APIs,  and web applications with Spring Boot and Spring Cloud. Here are a few of my projects: Project 1, Project 2, Project 3.",
+      "Spring is a Java-based framework used for building enterprise applications. I used it to create RESTful APIs,  and web applications with Spring Boot and Spring Cloud.",
   },
   {
     title: "MongoDB",
