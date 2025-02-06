25544762fdd11973fc9c05cbfca8fe4a04113fe7
Prelencos <120386410+Kadirleventkabadayi@users.noreply.github.com>
2025-02-06 13:07:52 +0300

    Refactor AnimCard component for cleaner structure and enhance responsive styles in globals.css

--- src/app/components/cards/AnimCard.tsx
+++ src/app/components/cards/AnimCard.tsx
@@ -5,27 +5,8 @@ interface AnimCardProps {
 
 const AnimCard: React.FC<AnimCardProps> = ({ imageSrc, position }) => {
   return (
-    <div
-      className={`card card-${position}`}
-      style={{
-        position: "relative",
-        width: "40%",
-        height: "360px",
-        borderRadius: "0.75em",
-        overflow: "hidden",
-        willChange: "transform",
-      }}
-    >
-      <img
-        className="card-image"
-        src={imageSrc}
-        alt={`Card ${position}`}
-        style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-        }}
-      />
+    <div className={`card card-${position}`}>
+      <img className="card-image" src={imageSrc} alt={`Card ${position}`} />
     </div>
   );
 };
