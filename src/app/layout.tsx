aa949ab6a6f1581ed5a27eba5a16619d5f0c21f0
Prelencos <120386410+Kadirleventkabadayi@users.noreply.github.com>
2025-02-25 12:05:45 +0300

    Add Open Graph metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,10 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Kadir Levent Kabadayı",
   description: "Kadir Levent Kabadayı's personal website 🍋",
+  openGraph: {
+    images:
+      "https://og-image-wheat.vercel.app/api/og?title=Kadir+Levent+Kabaday%C4%B1&description=Kadir+Levent+Kabaday%C4%B1%27s+personal+website&logoUrl=https%3A%2F%2Fraw.githubusercontent.com%2FKadirleventkabadayi%2FMy-WebSite%2Frefs%2Fheads%2Fmain%2Fpublic%2Fnewpp.png",
+  },
 };
 
 export default function RootLayout({
